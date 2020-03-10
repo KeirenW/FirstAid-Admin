91a342ae4de220cca5c9ab80b52e17df7deae823
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-10 23:41:14 +0000

    Added routing for users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { UsersComponent } from './users/users.component';
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'dispatch', component: DispatchComponent }
+  { path: 'dispatch', component: DispatchComponent },
+  { path: 'users', component: UsersComponent }
 ];
 
 @NgModule({
