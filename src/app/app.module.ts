239166220e0faab62b23469e2956976a7c09a8f3
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-10 23:39:48 +0000

    + Added routing + Added routerLinks to nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DispatchComponent } from './dispatch/dispatch.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'dispatch', component: DispatchComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DispatchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // for debugging only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
