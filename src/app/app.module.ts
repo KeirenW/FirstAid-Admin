2330728636ac66744f06d004047c82e94106928b
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-12 02:11:48 +0000

    Added activated routing for editing users and tidying up file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DispatchComponent } from './dispatch/dispatch.component';
 import { UsersComponent } from './users/users.component';
-
-import { environment } from 'src/environments/environment';
 import { UserCardComponent } from './users/user-card/user-card.component';
 import { EditUserComponent } from './users/edit-user/edit-user.component';
+
+import { environment } from 'src/environments/environment';
+
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'dispatch', component: DispatchComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:uuid', component: EditUserComponent }
 ];
 
 @NgModule({
