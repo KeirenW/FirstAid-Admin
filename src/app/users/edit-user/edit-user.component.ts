ecb770d7337d2fbdcb94c98a06d99e1556318d98
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-12 02:27:51 +0000

    Getting user by UUID using activated routes

--- src/app/users/edit-user/edit-user.component.ts
+++ src/app/users/edit-user/edit-user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-edit-user',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-user.component.css']
 })
 export class EditUserComponent implements OnInit {
+  private userUUID: string;
+  private user: Observable<any>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) {}
 
   ngOnInit(): void {
+    /**
+     *  Retrieve user data from database by their UUID.
+     *  Using ParamMap to get UUID from activated route.
+     */
+    this.user = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.firestore.collection('users').doc(params.get('uuid')).valueChanges()
+      )
+    );
+    this.user.subscribe(res => console.log(res));
   }
 
 }
