2db70e8d146d5d3905ed4e946791b2360a7dce19
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-15 21:00:55 +0000

    Moved getting of markers over from dashboard
    NOTE this will change

--- src/app/dashboard/ongoing-map/ongoing-map.component.ts
+++ src/app/dashboard/ongoing-map/ongoing-map.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { UserManagerService } from '../../services/user-manager/user-manager.service';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-ongoing-map',
@@ -7,12 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OngoingMapComponent implements OnInit {
   public mapDefaultPos: any;
+  public userLocations: Observable<any[]>;
 
-  constructor() {
+  constructor(private firestore: AngularFirestore, private userManager: UserManagerService) {
     this.mapDefaultPos = {
       latitude: 56.458110,
       longitude: -2.982118
     };
+
+    if (!firebase.auth().currentUser) {
+      userManager.createAnonymousUser();
+    }
+    this.userLocations = firestore.collection('users').valueChanges();
   }
 
   ngOnInit(): void {
