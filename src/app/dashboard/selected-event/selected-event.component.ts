2ae0abcf54e8cca1d34e6b5122cfe538a1a13321
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-15 20:46:52 +0000

    Added SelectedListComponent

---
+++ src/app/dashboard/selected-event/selected-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-event',
+  templateUrl: './selected-event.component.html',
+  styleUrls: ['./selected-event.component.css']
+})
+export class SelectedEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
