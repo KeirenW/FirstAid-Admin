1d20c0bffdda6c9f08f78913c68f115eaac1574a
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-15 20:51:47 +0000

    Dashboard map moved to ongoingMap component

--- src/app/dashboard/ongoing-map/ongoing-map.component.ts
+++ src/app/dashboard/ongoing-map/ongoing-map.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ongoing-map.component.css']
 })
 export class OngoingMapComponent implements OnInit {
+  public mapDefaultPos: any;
 
-  constructor() { }
+  constructor() {
+    this.mapDefaultPos = {
+      latitude: 56.458110,
+      longitude: -2.982118
+    };
+  }
 
   ngOnInit(): void {
   }
