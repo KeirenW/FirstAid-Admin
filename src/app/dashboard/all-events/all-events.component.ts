0c166e523fc9e2ef700b3252d21ce0c14c244cd8
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-16 02:28:36 +0000

    Can select which event to view and update

--- src/app/dashboard/all-events/all-events.component.ts
+++ src/app/dashboard/all-events/all-events.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { EventService } from 'src/app/services/event/event.service';
 
 @Component({
   selector: 'app-all-events',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-events.component.css']
 })
 export class AllEventsComponent implements OnInit {
+  events: Observable<any[]>;
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore, private eventService: EventService) {
+    this.events = this.firestore.collection('events').valueChanges();
+    this.events.subscribe(res => console.log(res));
+  }
 
   ngOnInit(): void {
   }
 
+  changeEvent(value) {
+    this.eventService.setCurrentEvent(value);
+  }
 }
--- src/app/dashboard/selected-event/selected-event.component.ts
+++ src/app/dashboard/selected-event/selected-event.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { IEvent } from 'src/app/interfaces/IEvent/ievent';
+import { EventService } from 'src/app/services/event/event.service';
 
 @Component({
   selector: 'app-selected-event',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selected-event.component.css']
 })
 export class SelectedEventComponent implements OnInit {
+  public uuid: Observable<any>;
+  public event: IEvent;
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore, private eventService: EventService) {
+    this.uuid = null;
+  }
 
   ngOnInit(): void {
+    this.eventService.selectedEvent.subscribe(res => {
+      if (res !== '') {
+        console.log('Getting event...');
+        this.uuid = this.firestore.collection('events').doc(res).valueChanges();
+
+        this.uuid.subscribe(doc => {
+          console.log('uuid updated');
+          this.event = {
+            Timestamp: doc.Timestamp,
+            UUID: doc.UUID,
+            Caller: doc.Caller,
+            Location: doc.Location,
+            Description: doc.Description,
+            Status: doc.Status,
+            Responder: doc.Responder,
+            Victim: {
+              Name: doc.Victim.Name,
+              Age: doc.Victim.Age,
+              Sex: doc.Victim.Sex
+            }
+          };
+          console.log(this.event);
+        });
+      }
+    });
   }
 
 }
