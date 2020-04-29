df048db6fd0fea975324d1b2500038bf179b6a61
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-04-29 18:13:19 +0100

    Added toast for when user is selected to confirm action

--- src/app/dashboard/selected-event/assign-responder/assign-responder.component.ts
+++ src/app/dashboard/selected-event/assign-responder/assign-responder.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AngularFireFunctions } from '@angular/fire/functions';
+import { AngularBootstrapToastsService } from 'angular-bootstrap-toasts';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-assign-responder',
@@ -14,7 +16,7 @@ export class AssignResponderComponent implements OnInit {
   public userCount = 9999;
   public distancesCalculated: boolean;
 
-  constructor(private fns: AngularFireFunctions) {
+  constructor(private fns: AngularFireFunctions, private toast: AngularBootstrapToastsService) {
     this.distancesCalculated = false;
   }
 
@@ -67,6 +69,36 @@ export class AssignResponderComponent implements OnInit {
   logValue() {
     console.log(this.distancesCalculated);
     console.log(this.usersWithDistance);
+  }
+
+  assignUser(user) {
+    console.log(user);
+    const toast = this.toast.showConfirmToast({
+      title: `Assign to this event?`,
+      text: `User: ${user.firstName} ${user.surname} has been chosen to respond to this event. Is this correct?`,
+      duration: 10000,
+      pauseDurationOnMouseEnter: true,
+      showProgressLine: true,
+      toolbarItems: {
+        actionButton: {
+          text: 'Confirm',
+          class: 'btn btn-success btn-sm',
+          visible: true
+        },
+        cancelButton: {
+          text: 'Cancel',
+          class: 'btn btn-danger btn-sm',
+          visible: true
+        }
+      }
+    });
+    const toastSubscription = toast.ConfirmationResult$.subscribe(value => {
+      console.log(`Toast confirm: ${value}`);
+      if (value === true) {
+        // Assign event, notify, set active to false
+      } // else don't need to do anything
+      toastSubscription.unsubscribe();
+    });
   }
 
 }
