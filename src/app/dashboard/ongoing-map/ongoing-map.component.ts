bc40c9e1cab44fd32fa24b1107fec368b41a52c2
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-05-04 00:30:38 +0100

    Location marker on map

--- src/app/dashboard/ongoing-map/ongoing-map.component.ts
+++ src/app/dashboard/ongoing-map/ongoing-map.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { UserManagerService } from '../../services/user-manager/user-manager.service';
-import * as firebase from 'firebase';
-import 'firebase/firestore';
+import { EventService } from 'src/app/services/event/event.service';
+import { AngularFireFunctions } from '@angular/fire/functions';
 
 @Component({
   selector: 'app-ongoing-map',
@@ -12,22 +10,38 @@ import 'firebase/firestore';
 })
 export class OngoingMapComponent implements OnInit {
   public mapDefaults: any;
-  public userLocations: Observable<any[]>;
+  public location: any;
 
-  constructor(private firestore: AngularFirestore, private userManager: UserManagerService) {
+  constructor(private firestore: AngularFirestore, private event: EventService, private ffns: AngularFireFunctions) {
     this.mapDefaults = {
       latitude: 56.458110,
       longitude: -2.982118,
       type: 'satellite'
     };
-
-    // if (!firebase.auth().currentUser) {
-    //   userManager.createAnonymousUser();
-    // }
-    // this.userLocations = firestore.collection('users').valueChanges();
+    this.location = null;
   }
 
   ngOnInit(): void {
+    this.event.selectedEvent.subscribe(res => {
+      if (res.length > 0) {
+        const test = this.firestore.collection('events').doc(res).valueChanges().subscribe(event => {
+          const eventDetails: any = event;
+          const eventLocation = eventDetails.Location;
+          console.log('Event', event);
+          const callable = this.ffns.httpsCallable('getLocation');
+          callable({
+            key: 'AIzaSyAdm8c2SnqvQGLUjsZCtCDKFNF4rorHeJM',
+            location: eventLocation
+          }).subscribe(result => {
+            this.location = JSON.parse(result);
+            this.location = this.location.results[0].geometry.location;
+            console.log(this.location);
+            test.unsubscribe();
+          });
+
+        });
+      }
+    });
   }
 
 }
